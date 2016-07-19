42539abd19fd14ffefb3216dff7ac183c5fe2207
Fahad <fahad@ipvisionsoft.com>
2016-07-19 10:48:26 +0600

    all file added

---
+++ codec/build/android/enc/jni/Android.mk
@@ -0,0 +1,10 @@
+
+LOCAL_PATH := $(call my-dir)
+MY_LOCAL_PATH := $(LOCAL_PATH)
+
+# Step3
+#Generate the libwelsdecdemo.so file
+include $(LOCAL_PATH)/welsencdemo.mk
+LOCAL_PATH := $(MY_LOCAL_PATH)
+LOCAL_LDLIBS := -llog
+
